9335a47ec580e1dbd3a8a285e724f73e29b9d383
Josthoon Rivas <josthoonrivas@gmail.com>
2024-11-01 21:46:16 -0300

    app-wear-app_wear_os_doma

---
+++ app/src/main/java/com/example/app_wear_os_doma/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.app_wear_os_doma.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun App_Wear_OS_DomaTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
